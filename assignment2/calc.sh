8420ddbc8a628ac6e653dfce514252830f544a5f
torjuskd <torjuskd@pompernel.ifi.uio.no>
2016-09-05 20:22:08 +0200

    first part of assignment2

--- assignment2/calc.sh
+++ assignment2/calc.sh
@@ -1,1 +1,20 @@
-#!/bin/bash
+#!/bin/bash -x
+option=$1;
+case "$option" in
+    -S)
+	#Sum 
+	;;
+    -P)
+	#Product
+	;;
+    -M)
+	#Maximum
+	;;
+    -m)
+	#Minimum
+	;;
+    *)
+	echo "$0: invalid option \"$option\""; exit ;;
+esac
+
+
