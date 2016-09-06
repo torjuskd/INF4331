886e1712b5fc6fa9872a1fa08914930dfec953a0
torjuskd <torjuskd@ifi.uio.no>
2016-09-06 10:24:21 +0200

    2.1 (calculator) finished

--- assignment2/calc.sh
+++ assignment2/calc.sh
@@ -1,20 +1,40 @@
-#!/bin/bash -x
-option=$1;
+#!/bin/bash
+option=$1; shift;
+declare -i result
+
 case "$option" in
     -S)
-	#Sum 
+	#Sum
+	result=0
+	for arg in $@; do
+	    ((result+=arg))
+	done
+	echo "$result"
 	;;
     -P)
 	#Product
+	result=1
+	for arg in $@; do
+	    ((result*=arg))
+	done
+	echo "$result"
 	;;
     -M)
 	#Maximum
+	result=$1; shift
+	for arg in $@; do
+	    [ "$arg" -gt "$result" ] && ((result=arg))
+	done
+	echo "$result"
 	;;
     -m)
 	#Minimum
+	result=$1; shift
+	for arg in $@; do
+	    [ "$arg" -lt "$result" ] && ((result=arg))
+	done
+	echo "$result"
 	;;
     *)
 	echo "$0: invalid option \"$option\""; exit ;;
 esac
-
-
