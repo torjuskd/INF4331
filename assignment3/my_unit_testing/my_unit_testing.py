c2adee971cdaed777593b7fb789ffd6e33a68430
torjuskd <torjuskd@ifi.uio.no>
2016-09-12 23:58:01 +0200

    assignment part 3.3 started

---
+++ assignment3/my_unit_testing/my_unit_testing.py
@@ -0,0 +1,9 @@
+#!usr/bin/env python3
+
+class UnitTest(object):
+    def __init__(self, func, args, kwargs, res):    # make test
+        NotImplemented          
+
+    def __call__(self):                             # run test
+        NotImplemented
+
