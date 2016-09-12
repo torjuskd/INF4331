540974f517f0c89fea901db520e4bff7d1c11e4a
torjuskd <torjuskd@ifi.uio.no>
2016-09-12 22:13:03 +0200

    assignment3 started, rpn.py done

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 ## INF3331-torjuskd
 #### Overview
 - Assignment1: Simply getting familiar with git
-- Assignment2: 3 bash scripts; calc.sh, clock.sh, subway.sh
+- Assignment2: 3 bash scripts; calc.sh (simple calculator), clock.sh (simple clock) and subway.sh (personal assistant; looks up subway departure times)
+- Assignment3: 4 python assignments: rpn.py (RPN-calculator), wc.py (word count-like script), my_unit_testing.py (simple unit testing) and a module of the unit testing file.
---
+++ assignment3/rpn.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# A simple calculator using a stack and reverse polish notation.
+# ex: ./rpn.py 1 1 +
+# > 2
+from math import sin
+from math import cos
+from math import sqrt
+import sys
+
+stack = []
+
+def calc(userinput):
+    if userinput == "p": #peek topmost item
+        print(stack[-1])
+
+    elif userinput == "v": #get square root
+        num1=stack[-1]
+        del stack[-1]
+        result=eval("sqrt("+num1+")")
+        print(result)
+        stack.append(result)
+
+    elif userinput == "sin": #find sin
+        num1=stack[-1]
+        del stack[-1]
+        result=eval("sin("+num1+")")
+        print(result)
+        stack.append(result)
+
+    elif userinput == "cos": #find cos
+        num1=stack[-1]
+        del stack[-1]
+        result=eval("cos("+num1+")")
+        print(result)
+        stack.append(result)
+
+    elif userinput in ("+", "-", "*", "/"):
+        num2 = stack[-1]
+        del stack[-1]
+        num1 = stack[-1]
+        del stack[-1]
+        result = eval(num1+" "+userinput+" "+num2)
+        stack.append(str(result))
+        print(result)
+        
+    elif userinput == "stack": #Added easter-egg - print stack
+        print(stack)
+
+    else:
+        num = userinput
+        stack.append(num)
+
+del sys.argv[0] #remove first arg (command used to execute script)
+for arg in sys.argv: #feed arguments into calc
+    calc(arg)
+
+while True: #keep taking inputs from keyboard
+    userinputList = input('> ')
+    for userinput in userinputList.strip().split(" "):
+        calc(userinput)
