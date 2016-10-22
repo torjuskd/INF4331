7b69f594b9258304c23766034f0a2ac7fcb3ca0e
torjuskd <torjuskd@ifi.uio.no>
2016-10-22 15:59:23 +0200

    5.1 finished

--- assignment5/highlighter.py
+++ assignment5/highlighter.py
@@ -3,26 +3,54 @@
 # outputs the file with colorized syntax to standard out.
 # Called with:
 # python3 highlighter.py <syntaxfile> <themefile> <sourcefile>.
+
 import sys
+import re
 
-
-def highlight(syntaxfile, themefile, sourcefile):
-    """Colorizes the file according to the specified rules
+def readrulefile(rulefilename, syntax=False):
+    """Reads rule-defining files in format <regex>: <rulename>
         
         Args:
-            syntaxfile (filename): File containing the syntax rules to use
-            themefile (filename): File containig the theme (colors, etc.) to use
+            rulefilename (filename): File containing the rules to process
+
+        Returns:
+            rules (list): List containing the rules to use
+        """
+    rules = []
+    with open(rulefilename) as rulefile:
+        for line in rulefile:
+            if syntax:
+                rules.append([re.search(r"^\"(.*)\":", line).group(1), re.search(r": (\w+)$", line).group(1)])
+            else:
+                rules.append(line.split(": "))
+    return rules
+
+def highlight(syntaxrules, themerules, sourcefilename):
+    """Colorizes the file according to the specified rules, and prints result to terminal
+        
+        Args:
+            syntaxrules (list): Containing the syntax rules to use
+            themerules (list): Containig the theme (colors, etc.) to use
             sourcefile (filename): File to highlight
 
         Returns:
             <none>
         """
-    
+    with open(sourcefilename) as sourcefile:
+        read_data = sourcefile.read()
+        data = read_data
+        for syntaxrule in syntaxrules:
+            for themerule in themerules:
+                if themerule[0] == syntaxrule[1]:
+                    data = re.sub("("+syntaxrule[0]+")", "\033[{}m".format(themerule[1]) + r"\1" + "\033[0m", data)
+    print(data)
 
 # Nice to have:
 if __name__ == "__main__":
     if len(sys.argv) < 4: # Print help
-        print("syntax: python3 highlighter.py <syntaxfile> <themefile> <sourcefile>")
+        print("Usage: python3 highlighter.py <syntaxfile> <themefile> <sourcefile_to_color>")
         sys.exit()
-    
-    highlight(syntaxfile=sys.argv[1], themefile=sys.argv[2], sourcefile=sys.argv[3])
+    syntaxrules = readrulefile(sys.argv[1], syntax=True)
+    themerules = readrulefile(sys.argv[2])
+    highlight(syntaxrules, themerules, sourcefilename=sys.argv[3])
+
