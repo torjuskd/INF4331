6903e61d7a6c94a1fa25ed544d1d6e41a6ebe8b4
torjuskd <torjuskd@ifi.uio.no>
2016-11-06 17:25:08 +0100

    everything seems to work now

--- assignment4/setup.sh
+++ assignment4/setup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 # Setup file for running setup-scripts
+#"master" setup script for compiling/installing all the scripts
 
-python3 ./mandelbrot_3_setup.py build_ext --inplace && sudo python3 ./setup.py install
+swig -python mandelbrot_4_swig.i
+sudo python3 mandelbrot_4_setup.py build_ext --inplace
+sudo python3 ./mandelbrot_3_setup.py build_ext --inplace
+sudo python3 ./setup.py install
